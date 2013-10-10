156978706826e22e26ffa5673c4b99f32ffa8936
Gareth Davies <gareth.davies@justchecking.co.uk>
2013-10-10 17:25:46 +0100

    Added ios detection via modernizr

--- js/panels.js
+++ js/panels.js
@@ -530,24 +530,6 @@ $(function() {
 
   });
 
-  //
-
-  document.addEventListener('touchmove', function(e) {
-
-    e.preventDefault(); 
-
-  }, false);
-
-  preventOverScroll(document.getElementById('page-wrapper'));
-
-  preventOverScroll(document.getElementById('panel-top'));
-
-  preventOverScroll(document.getElementById('panel-right'));
-
-  preventOverScroll(document.getElementById('panel-bottom'));
-
-  preventOverScroll(document.getElementById('panel-left'));
-
 });
 
 
